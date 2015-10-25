5383b24f18af160228d1941f2c33939cbb6f3f82
Haihong Wang <rainbow.wh@gmail.com>
2015-10-25 01:23:01 -0700

    Add tab bar navigation

--- Dropbox/settingsViewController.swift
+++ Dropbox/settingsViewController.swift
@@ -1,30 +1,30 @@
 //
-//  CreateAccountViewController.swift
+//  settingsViewController.swift
 //  Dropbox
 //
-//  Created by Haihong Wang on 10/24/15.
+//  Created by Haihong Wang on 10/25/15.
 //  Copyright © 2015 Haihong Wang. All rights reserved.
 //
 
 import UIKit
 
-class CreateAccountViewController: UIViewController {
+class settingsViewController: UIViewController {
 
+    @IBOutlet weak var settingsImageView: UIImageView!
+    @IBOutlet weak var settingsScrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //settingsScrollView.contentSize = settingsImageView.image!.size
+        settingsScrollView.contentSize = CGSize(width: settingsImageView.image!.size.width, height: settingsImageView.image!.size.height + 98)
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+       
         // Dispose of any resources that can be recreated.
-
     }
-
-    @IBAction func welcomeNewAccountButton(sender: AnyObject) {
-        navigationController!.popViewControllerAnimated(true)
-    }
+    
 
     /*
     // MARK: - Navigation
