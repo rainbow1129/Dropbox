fadebe2ed2fbd67f7ba65ece286b8d32e5ee5838
Haihong Wang <rainbow.wh@gmail.com>
2015-10-25 17:14:32 -0700

    Added dismissing modal transition for Terms

---
+++ Dropbox/TermsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TermsViewController.swift
+//  Dropbox
+//
+//  Created by Haihong Wang on 10/25/15.
+//  Copyright © 2015 Haihong Wang. All rights reserved.
+//
+
+import UIKit
+
+class TermsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func termsDoneButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
