9d29a0aee2c2526287e49ad57f558af0564fa64d
Haihong Wang <rainbow.wh@gmail.com>
2015-10-25 18:52:10 -0700

    changed segue

---
+++ Dropbox/TroubleSignInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TroubleSignInViewController.swift
+//  Dropbox
+//
+//  Created by Haihong Wang on 10/25/15.
+//  Copyright © 2015 Haihong Wang. All rights reserved.
+//
+
+import UIKit
+
+class TroubleSignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButton(sender: AnyObject) {
+        dismissViewControllerAnimated(false, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
